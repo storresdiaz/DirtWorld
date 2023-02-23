617142a0db36affa9f0998becdfa78509d488db9
Sael Omar Torres-Diaz <torresdiazsael@gmail.com>
2023-02-22 21:49:03 -0500

    Created menu screen

---
+++ core/src/com/dw/core/screens/ScreenMainMenu.java
@@ -0,0 +1,48 @@
+package com.dw.core.screens;
+
+import com.badlogic.gdx.Screen;
+import com.dw.core.Core;
+
+public class ScreenMainMenu implements Screen {
+
+    Core core;
+
+    public ScreenMainMenu(Core core){
+        this.core = core;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
