d7ba1ca25b149f1ccdc84c0d622ee9accb50648d
Sael Omar Torres-Diaz <torresdiazsael@gmail.com>
2023-02-24 13:34:46 -0500

    Added full screen mode. And added ESC input in Core.java to close app

--- core/src/com/dw/core/Core.java
+++ core/src/com/dw/core/Core.java
@@ -1,8 +1,6 @@
 package com.dw.core;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -25,6 +23,10 @@ public class Core extends Game {
 	public void render () {
 		ScreenUtils.clear(0, 0, 0, 1);
 		super.render();
+
+		if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)){
+			Gdx.app.exit();
+		}
 
 	}
 	
--- desktop/src/com/dw/core/DesktopLauncher.java
+++ desktop/src/com/dw/core/DesktopLauncher.java
@@ -1,7 +1,9 @@
 package com.dw.core;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Graphics;
 import com.dw.core.Core;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
@@ -9,6 +11,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
+		config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
 		config.setTitle("Dirt World");
 		new Lwjgl3Application(new Core(), config);
 	}
