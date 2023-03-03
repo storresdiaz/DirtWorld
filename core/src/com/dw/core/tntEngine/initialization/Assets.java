3e0b5de5cd2e51942b784dbc8c54a51290be79f1
Sael Omar Torres-Diaz <torresdiazsael@gmail.com>
2023-03-03 08:13:59 -0500

    Started creating the foundation for the Asset loader. Assets.class which will be used globally to manage game resources.

--- core/src/com/dw/core/tntEngine/initialization/Assets.java
+++ core/src/com/dw/core/tntEngine/initialization/Assets.java
@@ -1,5 +1,55 @@
 package com.dw.core.tntEngine.initialization;
 
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
+import java.util.HashMap;
+
 public class Assets {
 
+    public static AssetManager assetManager = new AssetManager();
+    private static boolean isFinishedLoading = false;
+    private static float progress = 0;
+
+    private HashMap<String, String> atlases;
+    public void initialize(){
+        initializeAtlases();
+    }
+
+    private void initializeAtlases(){
+        atlases = new HashMap<>();
+        atlases.put("entity-human", "assets/graphics/entities/entity-human.atlas");
+
+        for(String filePath : atlases.values()){
+            assetManager.load(filePath, TextureAtlas.class);
+        }
+
+    }
+
+    public void load(){
+        if(assetManager.update()){
+            setIsFinishedLoading(true);
+        }
+        setProgress(assetManager.getProgress());
+    }
+
+    public static AssetManager getAssetManager() {
+        return assetManager;
+    }
+
+    public static boolean isIsFinishedLoading() {
+        return isFinishedLoading;
+    }
+
+    private static void setIsFinishedLoading(boolean isFinishedLoading) {
+        Assets.isFinishedLoading = isFinishedLoading;
+    }
+
+    public static float getProgress() {
+        return progress;
+    }
+
+    public static void setProgress(float progress) {
+        Assets.progress = progress;
+    }
 }
